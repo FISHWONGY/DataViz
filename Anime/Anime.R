b0434767ffda8cf95469994fe10fd6a78e005119
WONGY66 <59711659+WONGY66@users.noreply.github.com>
2020-07-19 15:56:56 +0800

    Update Anime.R

--- Anime/Anime.R
+++ Anime/Anime.R
@@ -1,1 +1,17 @@
+```{r}
+library(readr)
+library(ggplot2)
+library(delabj)
+library(ggtext)
+library(paletteer)
+library(evoPalette)
+library(ghibli)
+library(patchwork)
+library(tidyverse)
+library(ggrepel)
+```
 
+
+```{r}
+anime <- read_csv(file="https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-23/tidy_anime.csv")
+```
