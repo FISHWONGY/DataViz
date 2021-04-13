6da3dc6e8040b65b73d7fba6a46cbcec071a3917
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2021-04-13 12:36:44 +0100

    Create scrapping.R

---
+++ GDPR/src/scrapping.R
@@ -0,0 +1,24 @@
+#Scrape the Privacy Affairs website
+
+library(rvest)
+link<- "https://www.privacyaffairs.com/gdpr-fines/"
+page <- read_html(link)
+temp <- page %>% html_nodes("script") %>% .[9] %>%
+rvest::html_text() 
+ends <- str_locate_all(temp, "\\]")
+starts <- str_locate_all(temp, "\\[")
+table1 <- temp %>% stringi::stri_sub(from = starts[[1]][1,2], to = ends[[1]][1,1]) %>%
+str_remove_all("\n") %>%
+str_remove_all("\r") %>%
+jsonlite::fromJSON()
+table2 <- temp %>% stringi::stri_sub(from = starts[[1]][2,2], to = ends[[1]][2,1]) %>%
+str_remove_all("\n") %>%
+str_remove_all("\r") %>%
+jsonlite::fromJSON()
+
+
+#Adding sources included the ICO as well, the Privacy Affairs is missing the fine for British Airlines & Marriot Inc.
+url <- "https://www.enforcementtracker.com/"
+gdpr_html <- read_html(url)
+gdpr2 <- html_table(gdpr_html, fill = TRUE)
+gdpr_tab <- gdpr2[[1]]
